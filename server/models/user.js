159d8b5769548ace24c8c69895b1bfbda84f1803
Pawel Rabinovic <trueone777@googlemail.com>
2017-12-31 17:03:24 +0100

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -54,6 +54,18 @@ UserSchema.methods.generateAuthToken = function () {
 
 };
 
+UserSchema.methods.removeToken = function (token) {
+  const user = this;
+
+  return user.update({
+      $pull: {
+          tokens: {
+              token
+          }
+      }
+  })
+};
+
 UserSchema.statics.findByToken = function (token) {
   const User = this;
 
