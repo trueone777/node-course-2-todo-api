a7a652489ebb22833ca5d51a97c2fcda70362256
Pawel Rabinovic <trueone777@googlemail.com>
2017-12-31 16:44:23 +0100

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -124,6 +124,19 @@ app.get('/users/me', authenticate,(req, res) => {
 });
 
 
+//POST /users/login {email, password}
+app.post('/users/login', (req, res) => {
+    const body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
 app.listen(port, () => {
     console.log('Started on port: ', port);
 });
