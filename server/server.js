f0c50315af497cb7d02dc05be67ec3bc3968c312
Pawel Rabinovic <trueone777@googlemail.com>
2018-01-21 22:27:58 +0100

    Refractoring some parts with async await

--- server/server.js
+++ server/server.js
@@ -85,8 +85,8 @@ app.delete('/todos/:id', authenticate, (req,res) => {
 
 });
 
-app.patch('/todos/:id',authenticate, (req, res) => {
-    let id = req.params.id;
+app.patch('/todos/:id',authenticate, async (req, res) => {
+    const id = req.params.id;
     let body = _.pick(req.body, ['text', 'completed']);
 
     if (!ObjectID.isValid(id)) {
@@ -100,31 +100,34 @@ app.patch('/todos/:id',authenticate, (req, res) => {
         body.completedAt = null;
     }
 
-    Todo.findOneAndUpdate({_id: id, _creator: req.user._id }, {
-        $set: body
-    }, {
-        new: true
-    }).then((todo) => {
-        if (!todo)
+    try {
+        const todo = await Todo.findOneAndUpdate({_id: id, _creator: req.user._id }, {
+            $set: body
+        }, {
+            new: true
+        });
+        if (!todo) {
             return res.status(404).send();
-
+        }
         res.send({todo});
-    }).catch((e) => res.status(400).send());
+    } catch (e) {
+        res.status(400).send();
+    }
 
 });
 
 //POST /users
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
 
-    let body = _.pick(req.body, ['email', 'password']);
-
-    let user = new User(body);
-
-    user.save().then(() => {
-        return user.generateAuthToken();
-    }).then((token) => {
+    try {
+        const body = _.pick(req.body, ['email', 'password']);
+        const user = new User(body);
+        await user.save();
+        const token = await  user.generateAuthToken();
         res.header('x-auth', token).send(user);
-    }).catch((e) => res.status(400).send(e));
+    } catch (e) {
+        res.status(400).send(e)
+    }
 });
 
 
@@ -134,25 +137,27 @@ app.get('/users/me', authenticate,(req, res) => {
 
 
 //POST /users/login {email, password}
-app.post('/users/login', (req, res) => {
-    const body = _.pick(req.body, ['email', 'password']);
+app.post('/users/login', async (req, res) => {
 
-    User.findByCredentials(body.email, body.password).then((user) => {
-        return user.generateAuthToken().then((token) => {
-            res.header('x-auth', token).send(user);
-        });
-    }).catch((e) => {
+    try {
+        const body = _.pick(req.body, ['email', 'password']);
+        const user =  await User.findByCredentials(body.email, body.password);
+        const token = await user.generateAuthToken();
+        res.header('x-auth', token).send(user);
+
+    } catch (e) {
         res.status(400).send();
-    });
+    }
 });
 
 
-app.delete('/users/me/token', authenticate, (req, res) => {
-    req.user.removeToken(req.token).then(() => {
+app.delete('/users/me/token', authenticate, async (req, res) => {
+    try {
+        await req.user.removeToken(req.token);
         res.status(200).send();
-    }, () => {
+    } catch (e) {
         res.status(400).send();
-    });
+    }
 });
 
 app.listen(port, () => {
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -126,7 +126,7 @@ describe('DELETE /todos/:id', () => {
                     return done(err);
 
                 Todo.findById(hexId).then((todo) => {
-                    expect(todo).toNotExist();
+                    expect(todo).toBeFalsy();
                     done();
                 }).catch((e) => done(e));
             });
@@ -144,7 +144,7 @@ describe('DELETE /todos/:id', () => {
                         return done(err);
 
                     Todo.findById(hexId).then((todo) => {
-                        expect(todo).toExist();
+                        expect(todo).toBeTruthy();
                         done();
                     }).catch((e) => done(e));
                 });
@@ -187,7 +187,8 @@ describe('PATCH /todos/:id', () => {
             .expect((res) => {
                 expect(res.body.todo.text).toBe('Updated todo');
                 expect(res.body.todo.completed).toBe(true);
-                expect(res.body.todo.completedAt).toBeA('number');
+               // expect(res.body.todo.completedAt).toBeA('number');
+                expect(typeof res.body.todo.completedAt).toBe('number');
             })
             .end(done);
     });
@@ -220,7 +221,7 @@ describe('PATCH /todos/:id', () => {
             .expect((res) => {
                 expect(res.body.todo.text).toBe('Updated todo 2');
                 expect(res.body.todo.completed).toBe(false);
-                expect(res.body.todo.completedAt).toNotExist();
+                expect(res.body.todo.completedAt).toBeFalsy();
             })
             .end(done);
     });
@@ -261,8 +262,8 @@ describe('POST /users', () => {
             .send({email, password})
             .expect(200)
             .expect((res) => {
-                expect(res.headers['x-auth']).toExist();
-                expect(res.body._id).toExist();
+                expect(res.headers['x-auth']).toBeTruthy();
+                expect(res.body._id).toBeTruthy();
                 expect(res.body.email).toBe(email);
             })
             .end((err) => {
@@ -271,8 +272,8 @@ describe('POST /users', () => {
                 }
 
                 User.findOne({email}).then((user) => {
-                    expect(user).toExist();
-                    expect(user.password).toNotBe(password);
+                    expect(user).toBeTruthy();
+                    expect(user.password).not.toBe(password);
                     done();
                 }).catch((e) => done(e));
             });
@@ -311,14 +312,14 @@ describe('POST /users/login', () => {
             })
             .expect(200)
             .expect((res) => {
-                expect(res.headers['x-auth']).toExist();
+                expect(res.headers['x-auth']).toBeTruthy();
             })
             .end((err, res) => {
                 if (err)
                     return done(err);
 
                 User.findById(users[1]._id).then((user) => {
-                    expect(user.tokens[1]).toInclude({
+                    expect(user.toObject().tokens[1]).toMatchObject({
                         access: 'auth',
                         token: res.headers['x-auth']
                     });
@@ -336,7 +337,7 @@ describe('POST /users/login', () => {
             })
             .expect(400)
             .expect((res) => {
-                expect(res.headers['x-auth']).toNotExist();
+                expect(res.headers['x-auth']).toBeFalsy();
             })
             .end((err, res) => {
                 if (err)
